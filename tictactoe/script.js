e7d834dbb0120aa3bba835a7bf47b5892f10bbf0
xfekke <fekke4201337@gmail.com>
2023-11-07 20:38:51 +0100

    Update

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,1 +1,39 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'x'
+
 const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+let circleTurn
+
+cellElements.forEach(cell => {
+  cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+
+  swapTurns()
+  setBoardHoverClass()
+
+
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList(X_CLASS)
+  board.classList(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
