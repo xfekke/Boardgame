6f5715c1f6245a096eb7d1cca538ee704d1011a1
s.sernald <fekke4201337@gmail.com>
2023-11-08 12:12:00 +0100

    update

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,5 +1,5 @@
 const X_CLASS = 'x'
-const CIRCLE_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,21 +10,27 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
 startGame()
 
-
+restartButton.addEventListener('click', startGame)
 
 function startGame() {
   circleTurn = false
   cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
     cell.addEventListener('click', handleClick, { once: true })
   })
   setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e) {
@@ -32,13 +38,28 @@ function handleClick(e) {
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   placeMark(cell, currentClass)
   if (checkWin(currentClass)) {
-
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
   }
+}
 
-  swapTurns()
-  setBoardHoverClass()
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
 
-
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
 }
 
 function placeMark(cell, currentClass) {
@@ -50,8 +71,8 @@ function swapTurns() {
 }
 
 function setBoardHoverClass() {
-  board.classList(X_CLASS)
-  board.classList(CIRCLE_CLASS)
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
   if (circleTurn) {
     board.classList.add(CIRCLE_CLASS)
   } else {
