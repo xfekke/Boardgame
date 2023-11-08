ce198590d0383dc9f7a08d4e86a19a3c1303e84c
s.sernald <fekke4201337@gmail.com>
2023-11-08 10:11:53 +0100

    Update script.js

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,18 +1,39 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'x'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 let circleTurn
 
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true })
-})
+startGame()
+
+
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+}
 
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+
+  }
 
   swapTurns()
   setBoardHoverClass()
@@ -36,4 +57,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS)
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
 }
