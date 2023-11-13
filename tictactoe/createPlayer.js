d779020eacc1f7aa6cd677cd4594d3c2d1f414a5
xfekke <fekke4201337@gmail.com>
2023-11-13 22:15:58 +0100

    Update

--- tictactoe/createPlayer.js
+++ tictactoe/createPlayer.js
@@ -1,17 +1,25 @@
-// import fs from "fs";
-// import users from './users.json'
-
-console.log('createPlayer.js loaded');
-
 function savePlayerBtn() {
-  let playerXName = document.getElementById('userNameX').value
-  let playerOName = document.getElementById('userNameO').value
+  let playerXName = document.getElementById('userNameX').value;
+  let playerOName = document.getElementById('userNameO').value;
 
-  console.log('playerXName:', playerXName);
-  console.log('playerOName:', playerOName);
+  // läs/hämta localstorage info
+  let existingData = localStorage.getItem('users');
 
+  if (!existingData) {
+    existingData = {};
+  } else {
+    existingData = JSON.parse(existingData);
+  }
 
+  // uppdatera
+  existingData.playerXName = playerXName;
+  existingData.playerOName = playerOName;
 
+  // spara till localstorage
+  localStorage.setItem('users', JSON.stringify(existingData));
+
+  console.log('Data saved successfully:', existingData);
 }
 
-savePlayerBtn()
+savePlayerBtn();
+
