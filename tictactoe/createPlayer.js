37ba6b3aa55e02311e17364b780307e268e8e714
xfekke <fekke4201337@gmail.com>
2023-11-13 21:53:57 +0100

    Updates

--- tictactoe/createPlayer.js
+++ tictactoe/createPlayer.js
@@ -1,46 +1,8 @@
 import fs from "fs";
 
-const data = JSON.parse(fs.readFileSync("users.json", "utf-8"))
+function savePlayerBtn() {
+  let playerXName = document.getElementById('userNameX').value 
+  let playerOName = document.getElementById('userNameO').value
 
-
-
-
-
-
-
-var userName = document.getElementById("userName");
-var jsonBtn = document.getElementById("jsonbtn");
-var jsonText = document.getElementById("jsontext");
-
-jsonBtn.addEventListener("click", function () {
-  // username objekt
-  var data = {
-    "userName": userName.value,
-  };
-
-  // läs json
-  fs.readFile('users.json', 'utf8', (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file:", err);
-      return;
-    }
-
-    // konvertera, gör tom array om json är tom
-    const existingData = jsonString ? JSON.parse(jsonString) : [];
-
-    // lägger till namn i array
-    existingData.push(data);
-
-    // sparar namn
-    fs.writeFile('users.json', JSON.stringify(existingData), 'utf8', (err) => {
-      if (err) {
-        console.log("Error writing file:", err);
-        return;
-      }
-
-      // Uppdatera användargränssnittet med JSON-data
-      jsonText.innerHTML = JSON.stringify(data);
-      console.log("User registered and data saved to users.json");
-    });
-  });
-});
+  localStorage.setItem('users', JSON.stringify('myStorage'));
+}
